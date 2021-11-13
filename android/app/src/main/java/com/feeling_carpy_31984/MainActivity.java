426690060277b8493162fe727f03f0cc8ac8595c
Crowdbotics <team@crowdbotics.com>
2021-11-13 22:40:40 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/feeling_carpy_31984/MainActivity.java
@@ -0,0 +1,15 @@
+package com.feeling_carpy_31984;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "feeling_carpy_31984";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "feeling_carpy_31984",
+  url: "https://feeling_carpy_31984.botics.co",
+  api: "https://feeling_carpy_31984.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
